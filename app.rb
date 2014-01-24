11e619591a26f6c6833c9d610e7e27c3fb243228
Mehul Kar <mehul.kar@gmail.com>
2014-01-24 01:47:31 -0800

    get all books from currently reading and render as json

--- app.rb
+++ app.rb
@@ -2,11 +2,18 @@ require 'sinatra'
 require 'sinatra/cross_origin'
 require 'open-uri'
 require 'nokogiri'
+require 'json'
 
 get '/' do
   cross_origin
   page = open("https://kindle.amazon.com/profile/Mehul-Kar/4275127/reading")
   nokogiri = Nokogiri::HTML(page)
-  book = nokogiri.css('#profilePage ul .profileBookEntry .title')[0].text
-  book
+  bookElements = nokogiri.css('#profilePage ul .profileBookEntry')
+
+  books = bookElements.map do |el|
+    author  = el.css('.author').text
+    title   = el.css('.title').text
+    { author: author, title: title }
+  end
+  return {books: books}.to_json
 end
